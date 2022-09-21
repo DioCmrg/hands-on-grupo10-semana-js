700e91dcde6264e74f53c8fb5693b66e50e0718a
Diovane Camargo <diovane.cmelo@gmail.com>
2022-09-21 11:00:39 -0300

    visualizacao dos eventos

---
+++ js/admin.js
@@ -0,0 +1,37 @@
+listaEventos = async (e) => {
+    url_base = "https://xp41-soundgarden-api.herokuapp.com"
+
+try {
+    var opcaoEventos = {
+        method: "GET",
+        redirect: "follow"
+    };
+    const resposta = await fetch(`${url_base}/events/`, opcaoEventos)
+
+    const conteudo = await resposta.json(); 
+
+    conteudo.forEach((e , i)=> {
+        const todosEventos = document.querySelector("tbody");
+
+        const novosEventos = document.createElement("tr");
+        novosEventos.innerHTML = `<th scope="row">${i+1}</th>
+        <td>${e.scheduled}</td>
+        <td>${e.name}</td>
+        <td>${e.atraction}</td>
+        <td>
+            <a href="reservas.html?id=${e._id}" class="btn btn-dark">ver reservas</a>
+            <a href="editar-evento.html?id=${e._id}&nome=${e.name}&banner=${e.poster}&descricao=${e.description}&data=${e.scheduled}&ingressos=${e.number_tickets}&atracoes=${e.attractions}"" class="btn btn-secondary">editar</a>
+            <a href="excluir-evento.html?id=${e._id}&nome=${e.name}&banner=${e.poster}&descricao=${e.description}&data=${e.scheduled}&ingressos=${e.number_tickets}&atracoes=${e.attractions}"" class="btn btn-danger">excluir</a>
+        </td>
+        </tr>`
+
+        todosEventos.appendChild(novosEventos);
+
+    });
+
+} catch(error) {
+    console.log(error);
+    alert("Não foi possível carregar os eventos")
+}
+}
+listaEventos()
