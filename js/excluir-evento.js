eb4a02230e3fc9126bc54122d7dae2a7d595b2ae
Nayara de Sousa <nayadsousa@gmail.com>
2022-09-21 21:02:23 -0300

    Merge branch 'master' of https://github.com/DioCmrg/hands-on-grupo10-semana-js

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,8 +1,3 @@
-const btn_excluir = document.querySelector(".btn-danger");
-const url_base = "https://xp41-soundgarden-api.herokuapp.com/events/";
-const parametros = new URLSearchParams(window.location.search);
-const idEvento = (window.location.search).split("?")[1];
-
 const nomeEvento = document.querySelector("#nome");
 const bannerEvento = document.querySelector("#banner");
 const atracoesEvento = document.querySelector("#atracoes");
@@ -10,38 +5,65 @@ const descricaoEvento = document.querySelector("#descricao");
 const dataEvento = document.querySelector("#data");
 const lotacaoEvento = document.querySelector("#lotacao");
 
+function limparHtml() {
+  nomeEvento.setAttribute("value", "");
+  bannerEvento.setAttribute("value", "");
+  atracoesEvento.setAttribute("value", "");
+  descricaoEvento.innerHTML = "";
+  dataEvento.setAttribute("value", "");
+  lotacaoEvento.setAttribute("value", "");
+}
+
+limparHtml();
+
+const btn_excluir = document.querySelector(".btn-danger");
+const url_base = "https://xp41-soundgarden-api.herokuapp.com/events/";
+
+const idEvento = window.location.search.split("?")[1];
+const nomeEventoId = "";
+
 async function excluirEvento() {
   try {
     const url = await fetch(`${url_base}`);
     const urlIdEvento = await url.json();
-    console.log("endpoint sem excluir:", urlIdEvento);
-  
-  urlIdEvento.forEach((evento, index)=>{
-    if (evento._id == idEvento){
-        const data = new Date (evento.scheduled);
+
+    console.log("resposta:", urlIdEvento);
+
+    urlIdEvento.forEach((evento, index) => {
+      if (evento._id == idEvento) {
+        const data = new Date(evento.scheduled);
         nomeEvento.setAttribute("value", `${evento.name}`);
         bannerEvento.setAttribute("value", `${evento.poster}`);
         atracoesEvento.setAttribute("value", `${evento.attractions}`);
-        descricaoEvento.innerHTML=`${evento.description}`;
+        descricaoEvento.innerHTML = `${evento.description}`;
         dataEvento.setAttribute("value", `${data.toLocaleDateString()}`);
         lotacaoEvento.setAttribute("value", `${evento.number_tickets}`);
-        console.log(index)
+        console.log(index);
 
-        nomeEventoExcluido = `${evento.name}`;
-    }
+        nomeEventoId = `${evento.name}`;
+      }
+    });
+  } catch (error) {
+    console.log(error);
   }
-} catch(erro) {
-  console.log(erro)
 }
+excluirEvento();
+
+async function excluir() {
+  try {
+    const res = await fetch(`${url_base}${idEvento}`, {
+      method: "DELETE",
+    });
+    console.log(res.status);
+    console.log("excluiu");
+    alert(`${nomeEventoId} Este evento foi excluído.`);
+    limparHtml();
+  } catch (error) {
+    console.log("Seu evento não foi deletado. Tente novamente.");
+  }
+}
+
+btn_excluir.onclick = (event) => {
+  event.preventDefault();
+  excluirParaSempre();
 };
-//     const deleteApi = await fetch(`${url_base}${id_evento}`, {
-//       method: "DELETE",
-//       redirect: "follow",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//   } catch (error) {
-//     console.log("Seu evento não foi excluído, tente novamente.");
-//   }
-// }
