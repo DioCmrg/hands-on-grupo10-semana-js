96ec378593c2d1581f69f93c19efd5990d8abb15
Diovane Camargo <diovane.cmelo@gmail.com>
2022-09-21 23:59:14 -0300

    Ajustes + conclusao do JS editar eventos

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
 listaEventos = async (e) => {
-    url_base = "https://xp41-soundgarden-api.herokuapp.com"
+url_base = "https://xp41-soundgarden-api.herokuapp.com"
 
 try {
     var opcaoEventos = {
@@ -17,7 +17,7 @@ try {
         novosEventos.innerHTML = `<th scope="row">${i+1}</th>
         <td>${e.scheduled}</td>
         <td>${e.name}</td>
-        <td>${e.atraction}</td>
+        <td>${e.attractions}</td>
         <td>
             <a href="reservas.html?id=${e._id}" class="btn btn-dark">ver reservas</a>
             <a href="editar-evento.html?id=${e._id}&nome=${e.name}&banner=${e.poster}&descricao=${e.description}&data=${e.scheduled}&ingressos=${e.number_tickets}&atracoes=${e.attractions}"" class="btn btn-secondary">editar</a>
